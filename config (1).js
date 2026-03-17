0f3bf7cbbbf9a6e30ee4bdd54c46b5100696a6a5
jasurasqarov04-cell <jasurasqarov04@gmail.com>
2026-03-17 10:18:37 +0500

    Add files via upload

---
+++ config (1).js
@@ -0,0 +1,6 @@
+// ─────────────────────────────────────────────────────
+//  Вставь сюда ID своей Google Sheets таблицы
+//  ID находится в URL: docs.google.com/spreadsheets/d/ВОТ_ЭТО/edit
+// ─────────────────────────────────────────────────────
+
+const SPREADSHEET_ID = '1_IDzcCd3P87oAzQopBCOXM3fOcyHnv2dxaDu_u3vjO0';
